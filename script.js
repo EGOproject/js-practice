7da9d04a8f60de64b6d3548b9e661ef5eacdff70
egoproject <enockmci@gmail.com>
2024-03-13 15:18:44 +0300

    ads

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let counter = 0
 function count(){
-    counter += 1
-    alert(counter);
+    if (document.querySelector("h2").innerHTML==="I HAVE LEFT"){
+        alert("NOT AVAILABLE PLEASE WAIT!");
+    }else {
+        counter += 1
+    // alert(counter);
+        const countValue = document.querySelector("h1").innerHTML = counter
+    }
+    
 }
 function hello(){
     // alert("hello, world");
     if (document.querySelector("h2").innerHTML === "I AM HERE"){
-        document.querySelector("h2").innerHTML = "I HAVE LEFT";
+        document.querySelector("h2").innerHTML = "I HAVE LEFT not";
     } else{
         document.querySelector("h2").innerHTML = "I AM HERE";
+    }
+}
+function helloModified(){
+    // creating variables for functions
+    let heading = document.querySelector("h2");
+    //instead of let you can use "const" to make it unchangeable
+    if (heading.innerHTML === "I AM HERE"){
+        heading.innerHTML = "I HAVE LEFT";
+    } else{
+        heading.innerHTML = "I AM HERE";
     }
 }
