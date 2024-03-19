b40d7f30cbb840a65bd9b1309ab3e0ee79c4f0b6
egoproject <enockmci@gmail.com>
2024-03-19 12:48:38 +0300

    end of cs50

---
+++ currency.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("https://api.exchangeratesapi.io/latest?base=USD")
+    .then(response => response.json())
+    .then(data => {console.log(data);
+    });
+})
+
+// have to learn APIs
